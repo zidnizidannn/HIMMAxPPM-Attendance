c1abf15d3816b6666990704ecd26b855189b0c04
Zidni Zidan Mahestra Setyawan <zidnizidandmc@gmail.com>
2023-11-21 21:32:36 +0700

    regitrasi.php done, but need a little improve haha

--- registrasi.php
+++ registrasi.php
@@ -8,41 +8,55 @@ $dbname = "sakuhimma";
 
 $db = new mysqli($servername, $username, $password, $dbname);
 
-// Terima data dari form
-$username = $_POST['userName'];
-$no_wa = $_POST['noWa']; 
-$password = $_POST['pass'];
-
-if(empty($username)){
+// ======== register ========
+$usernameReg = $_POST['userName'];
+$no_waReg = $_POST['noWa']; 
+$passwordReg = $_POST['pass'];
+if(empty($usernameReg)){
     echo "Username tidak boleh kosong";
     exit();
 }
-
-if(empty($no_wa)){
+if(empty($no_waReg)){
     echo "No. Whatsapp tidak boleh kosong";
     exit();
 } 
-
-if(empty($password)){
+if(empty($passwordReg)){
     echo "Password tidak boleh kosong";
     exit();
 }
-
-// Enkripsi password
-$password = md5($password); 
-
-// Query insert data ke database
-$query = "INSERT INTO loginuser VALUES ('', '$username', '$no_wa', '$password')";
-
+$passwordReg = md5($passwordReg); 
+$query = "INSERT INTO loginuser (userName, noWhatsapp, pass) VALUES ('$usernameReg', '$no_waReg', '$passwordReg')";
 if($db->query($query)){
-
-  // Berhasil insert, redirect ke halaman utama
-    header('Location: index.html');
-
+    header('Location: coba.html');
 } else {
-
     echo "Gagal insert data";
+}
+mysqli_close($db);
 
+
+
+// ======== login ========
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+$usernameLogin = $_POST['username']; 
+$passwordLogin = $_POST['pass'];
+
+$sql = "SELECT * FROM loginuser WHERE userName='$usernameLogin'";
+
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    $row = mysqli_fetch_assoc($result);
+    if (md5($passwordLogin) === $row['pass']) {
+        $_SESSION['user_id'] = $row['id'];
+        header("Location: coba.html"); 
+    }
+    else {
+        echo "Password salah!";
+    }
+} else {
+    echo "User tidak ditemukan!";  
 }
 
+mysqli_close($conn);
 ?>
